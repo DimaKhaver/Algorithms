48c1bfc06ead04b6467adbaaf010c9d8db36fec3
DimaKhaver\Dima <xaverdima@gmail.com>
2017-08-30 14:52:37 +0300

    Working with simple data structures

---
+++ src/data_structures_and_algorithms_book/stacks_and_queues/PriorityQueue.java
@@ -0,0 +1,56 @@
+package data_structures_and_algorithms_book.stacks_and_queues;
+
+public class PriorityQueue {
+
+    // Simple array-based priority queue
+
+    private int maxSize;
+    private long [] queArray;
+    private int items;
+
+    public PriorityQueue(int s) {
+        maxSize = s;
+        queArray = new long[maxSize];
+        items = 0;
+    }
+
+    public void insert(long item) {
+        int j = 0;
+
+        if (items == 0)
+            queArray[items++] = item;
+        else
+            for (j = items - 1; j >= 0; j--) {
+                if (item > queArray[j])
+                    queArray[j+1] = queArray[j];
+                else
+                    break;
+            }
+
+        queArray[j+1] = item;
+        items++;
+    }
+
+    public long remove() {
+        return queArray[--items];
+    }
+
+    public long peekMin() {
+        return queArray[items - 1];
+    }
+
+    public boolean isEmpty() {
+        return (items == 0);
+    }
+
+    public boolean isFull() {
+        return (items == maxSize);
+    }
+
+
+
+
+
+
+
+}
---
+++ src/data_structures_and_algorithms_book/stacks_and_queues/Reverser.java
@@ -0,0 +1,4 @@
+package data_structures_and_algorithms_book.stacks_and_queues;
+
+public class Reverser {
+}
