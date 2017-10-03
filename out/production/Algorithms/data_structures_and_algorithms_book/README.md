379eb71900f30a38a87a37c26196bd39ad990b90
DimaKhaver\Dima <xaverdima@gmail.com>
2017-10-03 17:41:07 +0300

    Added more elegant implementation of binary search

--- out/production/Algorithms/data_structures_and_algorithms_book/README.md
+++
@@ -1,3 +0,0 @@
-# Data Structures and Algorithms in Java(2ed)
-It's a book I am currently going through and committing my programming projects.
-(If you find any mistakes, please point them out -  I will fix)
--- src/data_structures_and_algorithms_in_java/simple_sorting/BinarySearch.java
+++ src/data_structures_and_algorithms_in_java/simple_sorting/BinarySearch.java
@@ -1,4 +1,30 @@
 package data_structures_and_algorithms_in_java.simple_sorting;
 
-public class BinarySearch {
+class BinarySearch {
+
+    private int[] data;
+    private int size;
+
+    private BinarySearch() {
+    }
+
+    private boolean binarySearch(int key) {
+        int low = 0;
+        int high = size - 1;
+
+        while (low <= high) {
+            int middle = (low + high) / 2;
+
+            if (data[middle] == key)
+                return true;
+
+            if (data[middle] < key)
+                low = middle + 1;
+
+            if (data[middle] > key)
+                high = middle - 1;
+        }
+        return false;
+    }
+
 }
